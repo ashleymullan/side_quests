04901126f027361c7b8a8c1a333d77564234be2f
Ashley Mullan <54551344+ashleymullan@users.noreply.github.com>
2026-01-26 20:38:30 -0600

    tweak bingo script

--- bingo_makR.R
+++ bingo_makR.R
@@ -6,15 +6,15 @@ library(googlesheets4) #for data pulling
 library(ggpubr) #for creating the bottom piece
 library(png) #for reading in the bottom piece
 
-#put your lists of prompts (at least 25, at most 50) in column A of a Google
-#Sheet. name the sheet "bingo" and make cell A1 "bingo_prompts"
-#the below is a sample sheet, feel free to duplicate
+# put your lists of prompts (at least 25, at most 50) in column A of a Google
+# Sheet. name the sheet "bingo" and make cell A1 "bingo_prompts"
+# the below is a sample sheet, feel free to duplicate
 prompt_url <- paste0("https://docs.google.com/spreadsheets/d/1nj2IJ-",
                      "imAo89ih4wjVZ_VAZSXoPy9RDaJIZHba73Uuw/edit?",
                      "gid=482537420#gid=482537420")
 
-#replace me with the image you want to go beneath your bingo board
-#must be saved locally, not a URL
+# replace me with the image you want to go beneath your bingo board
+# must be saved locally, not a URL
 image_path <- "image_here.png"
 
 prompts <- read_sheet(prompt_url,
@@ -42,6 +42,9 @@ bingo_font_size <- 8
 num_distinct_boards <- 1 #change me to generate more boards
 save_path <- "replace_me_with_directory_for_finished_boards" 
 
+# run this loop to save a folder full of distinct bingo boards. 
+# I then recommend https://www.ilovepdf.com/merge_pdf to make one file 
+# so you can print in one shot
 for(i in 1:num_distinct_boards){
   game_board <- data.frame(rows, cols,
                           wrapped = sample(str_wrap(prompts, 12), 25)) |>
@@ -69,7 +72,7 @@ for(i in 1:num_distinct_boards){
   } else {
     bingo <- game_board
   }
-  file_name <- paste0(save_path, "/bingo", i, ".png")
+  file_name <- paste0(save_path, "/bingo", i, ".pdf")
   ggsave(file_name, plot = bingo)
 }
 
